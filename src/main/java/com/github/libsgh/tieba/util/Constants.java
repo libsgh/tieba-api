a5789bf15c107aa2e1d251c81d38212af82b0efc
libsgh <woiyyng@gmail.com>
2019-05-06 14:54:51 +0800

    新增签名档获取和删除接口
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- src/main/java/com/github/libsgh/tieba/util/Constants.java
+++ src/main/java/com/github/libsgh/tieba/util/Constants.java
@@ -60,4 +60,6 @@ public class Constants {
 	public static final String FORUM_SUPPORT = "http://tieba.baidu.com/celebrity/submit/support";//2。名人堂助攻
 	public static final String LEBO_API = "http://leboapi.baidu.com/leboapi/user?type=getUserDetail";//BDUSS查询用户信息
 	public static final String GET_USER_PROFILE = "http://ce.baidu.com/site/getUserProfile";//BDUSS查询用户名和头像
+	public static final String SIGN_DEL = "http://tieba.baidu.com/f/user/cm/sign_del";//删除签名档
+	public static final String SIGN_QUERY = "http://tieba.baidu.com/f/user/sign";//获取签名档
 }
